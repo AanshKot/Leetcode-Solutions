fe64439076da10c4e6c565b87048799150c41966
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-05-11 21:38:08 -0400

    Time: 21 ms (50.18%), Space: 12.5 MB (51.9%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -8,26 +8,28 @@ class Solution(object):
         
         res = []
 
-        def dfs(i, cur, curSum):
-            if curSum > target:
-                return
-            if i >= len(candidates) and curSum != target:
-                return
-            
-            if curSum == target:
-                res.append(list(cur))
+        def dfs(i, curSubset, curSum):
+            # final case: reached end of candidates array, curSum is greater than target curSubset cannot be appended to result
+            if(i >= len(candidates) or curSum > target):
                 return
             
-            #left decision
-            cur.append(candidates[i])
-            dfs(i, cur,curSum + candidates[i])
+            # second case: curSubset has numbers equal to target append to res
+            if(curSum == target):
+                res.append(list(curSubset))
+                return
+            
+            #left decision: choose to stay at element and keep adding to subset
+            curSubset.append(candidates[i])
+            dfs(i, curSubset, curSum + candidates[i])
 
-            #right decision
-            cur.pop()
-            dfs(i+1, cur, curSum)
+            #right decision: choose to not include element and move on 
+            curSubset.pop()
+            dfs(i + 1, curSubset, curSum)
 
         dfs(0, [], 0)
+
         return res
+
 
 
             
