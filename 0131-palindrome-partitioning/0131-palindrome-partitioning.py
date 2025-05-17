2893c4a48a76c8938dd738d08fd28066dafa3aac
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-05-16 21:44:31 -0400

    Time: 80 ms (63.08%), Space: 50.4 MB (7.47%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -7,32 +7,26 @@ class Solution(object):
 
         res = []
 
-        part = []
+        subset = []
 
         def dfs(i):
-            #know that we have a valid partition and no more characters to add
             if i >= len(s):
-                res.append(list(part))
-                return
+                res.append(list(subset))
+                return 
 
-            #every possible subtring, checking if its palindrome then doing dfs
             for j in range(i, len(s)):
                 if self.isPalindrome(s, i, j):
-                    part.append(s[i: j+1])
-                    dfs(j+1)
+                    subset.append(s[i: j + 1])
+                    dfs(j + 1)
 
-                    #right decision
-                    part.pop()
+                    subset.pop()
         dfs(0)
         return res
 
-    # s only contains lowercase english letters
-    # is S a palindrome if it starts at index i and ends at index j
-    def isPalindrome(self,s, low, high):
+    def isPalindrome(self, s, low, high):
         while low < high:
             if s[low] != s[high]:
                 return False
             low += 1
             high -= 1
         return True
-        
