b35ed835d058a817ad706d451521fb6bf6824266
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-05-22 22:27:04 -0400

    Time: 42 ms (25.89%), Space: 13.2 MB (42.54%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -13,22 +13,34 @@ class Solution(object):
         :type head: Node
         :rtype: Node
         """
-        oldToCopy = { None: None }
+        oldToCopy = { None: None } #base case where recieved node to be copied is None
 
+        #first pass
         cur = head
-        
-        #first pass, cloning nodes
+
         while cur:
-            copy = Node(cur.val)
-            oldToCopy[cur] = copy #map old node to copy node
+            #1. copy the node to a new node
+            #2. update the hashmap to map the old node to the copied
+            #3. now the old node maps to the deep copied node 
+            copiedNode = Node(cur.val)
+            oldToCopy[cur] = copiedNode
+            cur = cur.next
+            #second pass
+            
+        #the old node's pointers act as the key for the values stored in the copied node
+        cur = head
+
+        while cur:
+            #1. copy.next = oldToCopy[cur.next]
+            #2. copy.random = oldToCopy[cur.random]
+
+            #retrieve the copied node
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
             cur = cur.next
 
-        #second pass
-        cur = head
-        while cur:
-            copy =  oldToCopy[cur]
-            copy.next = oldToCopy[cur.next] #the old node's next pointer is the key, the value is the copied old node's next pointer
-            copy.random = oldToCopy[cur.random] #the old node's random pointer is the key that has the copied old node's random pointer
-            cur = cur.next
+        return oldToCopy[head] #head is an old node, there exists a copy of it
 
-        return oldToCopy[head] #head of old list is copied in hashmap and will be head of new list
+
+
