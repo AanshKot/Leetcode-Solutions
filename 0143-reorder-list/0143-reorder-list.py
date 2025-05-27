1d0645cb6d586a3d300e459baa51b8144357edda
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-05-26 22:13:26 -0400

    Time: 5 ms (78.5%), Space: 30.2 MB (64.56%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -10,23 +10,20 @@ class Solution(object):
         :rtype: None Do not return anything, modify head in-place instead.
         """
         
-        # first find halway point
         slow = head
         fast = head
 
         while fast.next and fast.next.next:
-            slow = slow.next
             fast = fast.next.next
+            slow = slow.next
 
-        #slow pointer is at our halway point, everything after the slow pointer is the second list
-        #pass in slow.next to reverse list, start from head of original list to slow
-        #set slow.next to null
-
+        #slow is at the midpoint
         temp = slow.next
         slow.next = None
-
         reversedList = self.reverseList(temp)
+
         listRunner = head
+        
         while listRunner and reversedList:
             temp = listRunner.next
             listRunner.next = reversedList
@@ -38,15 +35,14 @@ class Solution(object):
         return head
 
     def reverseList(self, head):
-        l = head
-        r = None
-        while l:
-            temp = l.next
-            l.next = r
-            r = l
-            l = temp
+        listRunner = head
 
-        return r
+        left = None
 
-
-
+        while listRunner:
+            temp = listRunner.next
+            listRunner.next = left
+            left = listRunner
+            listRunner = temp
+        
+        return left
