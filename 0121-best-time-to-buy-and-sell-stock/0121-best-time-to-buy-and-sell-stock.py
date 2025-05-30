89cec51dc70e8b7063828339e51051c56585aa4e
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-05-29 20:52:23 -0400

    Time: 127 ms (27.63%), Space: 19 MB (71.87%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        if len(prices) == 1:
+            return 0
+
+        maxProfit = 0
+        left = 0
+        right = 1
+
+        while right < len(prices):
+            if(prices[left] < prices[right]): #if price on earlier day is less than price in future
+                maxProfit = max(maxProfit, prices[right] - prices[left])
+                #keep left pointer at the same day
+
+            elif(prices[left] >= prices[right]):
+                left = right
+            
+            right += 1
+        
+        return maxProfit
+
+
+            
