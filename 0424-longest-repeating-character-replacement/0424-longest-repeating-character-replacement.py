fece2713d2a3184c6bdef43e375289425331be9a
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-06-06 22:06:56 -0400

    Time: 308 ms (13.72%), Space: 15.4 MB (47.06%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,29 +5,30 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        
-        count = {}
-        res = 0
 
         l = 0
+        res = 0
+        freqMap = {}
 
+        # len of sliding window: r - l + 1
         for r in range(len(s)):
-            #update number of appearances of a character
-            # count[s[r]] = 1 + count.get(s[r], 0) # this is Neetcode's way
-            if s[r] in count:
-                count[s[r]] += 1
+            freqMap[s[r]] = 1 + freqMap.get(s[r],0)
 
-            else:
-                count[s[r]] = 1
+            dominantCharFreq = 0
+            for i in freqMap:
+                dominantCharFreq = max(dominantCharFreq, freqMap[i])
+                
+            # windowLength - the dominant character frequency of appearances > k then we want to reduce the length of the window until it reaches valid state
+            while (r - l + 1) - dominantCharFreq  > k:
+                # move our window right
+                # 1. decrement the frequency of the character we are leaving out (s[l])
+                # 2. move the left pointer to the right by 1
 
-            #number of replacements > k
-            while ((r - l + 1) - max(count.values())) > k:
-                #1. decrement number of appearances of left char
-                #2. increment left pointer
-                count[s[l]] -= 1
+                freqMap[s[l]] = freqMap[s[l]] - 1
                 l += 1
 
-
-            res = max(res, r - l + 1) #max of result and size of window, updated after checking is window is valid
+            res = max(res, r - l + 1)
 
         return res
+
+
