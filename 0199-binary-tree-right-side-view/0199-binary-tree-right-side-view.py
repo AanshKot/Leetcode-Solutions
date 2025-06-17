d9ca025e8489d6b8ddb19c20ab1e13405a199c1f
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-06-16 22:21:19 -0400

    Time: 0 ms (100%), Space: 17.9 MB (37.68%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -9,6 +9,12 @@ class Solution:
         if not root:
             return []
         
+        #1. BFS (level order traversal)
+        #2. append the rightmost element in the level to the res
+            #a. the last element in the level is guaranteed to be the rightmost element in the tree
+                #this is due to the fact that the level order traversal starts from the leftmost node and 
+                #goes to the rightmost existing node
+
         q = deque()
         q.append(root)
         
@@ -26,7 +32,7 @@ class Solution:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
-            
+            #the rightmost element is 
             rightmostNodeVals.append(level[-1])
 
         return rightmostNodeVals
