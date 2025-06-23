abf812741554a4d76ac2187b685b3cf9363e147f
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-06-23 17:46:06 -0400

    Time: 8 ms (24.52%), Space: 20.8 MB (82.61%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -1,31 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def diameterOfBinaryTree(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        res = [0] #store our max diameter
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = [0]
 
         def dfs(root):
-
             if not root:
                 return 0
             
-            #for each of the nodes we want the diameter and the height of the tree
-            
-            leftHeight = dfs(root.left) #calculate the left subtree height
-            rightHeight = dfs(root.right)
+            leftHeight = dfs(root.left) #maxDepth of leftSubtree
+            rightHeight = dfs(root.right) #maxDepth of rightSubtree
 
-            #compare diameter to stored cur max: res
-            res[0] = max(res[0], leftHeight + rightHeight)
+            res[0] = max(res[0], leftHeight + rightHeight) #combined leftHeight + rightHeight gives max diameter
 
-            return 1 + max(leftHeight,rightHeight) #find the max height of left and right subtree
+            return 1 + max(leftHeight,rightHeight) #return the max depth of the subtree
 
         dfs(root)
-        return res[0] 
+        return res[0]
