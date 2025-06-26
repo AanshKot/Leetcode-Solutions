52b746643ab00005102bf2e937a7cc067d352790
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-06-26 15:27:23 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -1,41 +1,37 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isSubtree(self, root, subRoot):
-        """
-        :type root: Optional[TreeNode]
-        :type subRoot: Optional[TreeNode]
-        :rtype: bool
-        """
-        #subRoot == NULL means its a subset of all possible trees
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if not subRoot:
             return True
-
-        #root == NULL and subRoot existing => Null cannot superset anything
         if not root:
             return False
         
-        if (self.sameTree(root, subRoot)):
-            return True
-        
-        left = self.isSubtree(root.left, subRoot)
-        right = self.isSubtree(root.right, subRoot)
+        leftSubtree = self.isSameTree(root.left, subRoot)
+        rightSubtree = self.isSameTree(root.right, subRoot)
 
-        return left or right
+        return leftSubtree or rightSubtree
 
-    def sameTree(self, p, q):
+    def isSameTree(self, p, q):
         if not p and not q:
             return True
-        if (p and not q) or (q and not p):
+            
+        if not p and q:
             return False
+
+        if not q and p:
+            return False
+        
         if p.val != q.val:
             return False
         
-        leftSubtree = self.sameTree(p.left, q.left)
-        rightSubtree = self.sameTree(p.right, q.right)
+        leftSubtree = self.isSameTree(p.left, q.left)
+        rightSubtree = self.isSameTree(p.right, q.right)
 
         return leftSubtree and rightSubtree
+
+        
