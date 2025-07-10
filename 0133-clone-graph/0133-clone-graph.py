a1a6a8ba5ea92f61e799ecda6d4888d048264e94
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-07-10 10:56:34 -0400

    Time: 28 ms (99.51%), Space: 18.4 MB (16.19%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,40 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+
+        cloneHash = {}
+
+        if not node:
+            return None
+
+        def dfs(node):
+            #the base case is that the node has already been traversed and cloned
+            #in this case we want to return the copied node
+            # return the copied node
+            if node in cloneHash:
+                return cloneHash[node]
+            
+            clone = Node(node.val)
+            cloneHash[node] = clone
+
+            #iterate over the original node's neighbours preform dfs over them to get the copied neighbour node not the original neighbour node 
+            for neighbour in node.neighbors:
+                #dfs responsible for creating the clone
+                #dfs also responsible for creating the copied neighbours 
+                clone.neighbors.append(dfs(neighbour))
+            
+            return clone
+
+
+        return dfs(node)
+           
+
+        
