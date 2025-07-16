9fc01da60868d7321362951b3f51dadc25daefbb
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-07-16 15:00:51 -0400

    0207-COURSE-SCHEDULE Stats: Time: 7 ms (46.42%), Space: 19.3 MB (51.69%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,45 +1,44 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # first iteration
-        graphAdjList = {}
+        # create directed graph in the form of an adjacency list
+        preMap = { i:[] for i in range(numCourses) }
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        print(preMap)
 
-        for i in range(numCourses):
-            graphAdjList[i] = set()
+        visited = set()
+        def dfs(course):
+            if course in visited:
+                return False #cycle detected unable to take course schedule
 
-
-        for courseDependency in prerequisites:
-            graphAdjList[courseDependency[0]].add(courseDependency[1])
-        
-        #we have adjacency list how do we detect a cycle
-        recursionStack = [False] * numCourses
-        visited = [False] * numCourses
-        
-        # graph has a cycle if there is a node that has an edge to an ancestor in the DFS recursive stack
-        def detectCycleDFS(adjacencyList, curNode, visited, recursionStack):
-            if recursionStack[curNode]:
+            if preMap[course] == []:
                 return True
             
-            if visited[curNode]: #if the node has already been visited and not in the recursion stack 
-               return False
+            visited.add(course)
 
-            visited[curNode] = True
-            recursionStack[curNode] = True
-
-            #check if every node 
-            for node in adjacencyList[curNode]:
-                if(detectCycleDFS(adjacencyList, node, visited, recursionStack)):
-                    return True
-            
-            recursionStack[curNode] = False
-            return False
+            for pre in preMap[course]:
+                if not dfs(pre):
+                    return  False
         
-        for i in graphAdjList:
-            if detectCycleDFS(graphAdjList, i, visited, recursionStack):
-                return False
+            # if we exit the for loop inside dfs we know that the course is a good node without cycles
+            # we remove it from visited so other paths can successfully revisit it
+            # we can visit the node twice without it being a cycle due to it terminating multiple paths
+            visited.remove(course)
+            preMap[course] = []
 
+            return True
+
+        # if we recieve a disconnected graph i.e. 1 -> 2 and 3 -> 4
+        for course in range(numCourses):
+            #if cycle detected in DFS we know we cant take this course schedule
+            if not dfs(course):
+                return False
         return True
+
+
+
+
             
-          
 
 
 
