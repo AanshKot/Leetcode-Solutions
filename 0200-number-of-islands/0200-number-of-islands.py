8f48a25eab0b2451b2efa558a2f67c3d9beae3e3
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-07-21 14:45:57 -0400

    0200-NUMBER-OF-ISLANDS Stats: Time: 317 ms (10.22%), Space: 25.3 MB (26.85%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,55 +3,60 @@ class Solution:
         if len(grid[0]) == 0:
             return 0
 
-        #background
+        #Constants:
+        maxRows = len(grid)
+        maxCols = len(grid[0])
+        
+        #visited set
         visited = set()
 
-        rows = len(grid)
-        cols = len(grid[0])
+        # number of islands
         numIslands = 0
 
-        def bfs(row,col): 
+        # directions R -> L -> U -> D
+        directions = [[0,1], [0, -1], [-1, 0], [1,0]]
+
+        def bfs(landTileRow, landTileCol):
             q = deque()
-            # if no diagonal connections (Which is the case)
-            directions = {'bottom': [0,1], 'right': [1,0], 'top': [-1, 0], 'left': [0, -1]}
 
-            q.append((row,col))
-            visited.add((row,col))
+            q.append((landTileRow, landTileCol))
 
-            while len(q) > 0:
+            while q:
                 curRow, curCol = q.popleft()
 
-                # if the node is a 1 and hasn't been visited
-                # increase the number of islands
-                # add the nodes unvisited neighbours to the queue
-                # only if they are in bounds
-                # mark as visited
-                    
-                for direction in directions:
-                    newRow = curRow + directions[direction][0]
-                    newCol = curCol + directions[direction][1]
+                visited.add((curRow, curCol))
 
-                    if(newRow in range(rows) 
-                        and newCol in range(cols) 
-                        and (newRow, newCol) not in visited
-                        and grid[newRow][newCol] == '1'
+                for dRow, dCol in directions:
+                    newRow = curRow + dRow
+                    newCol = curCol + dCol
+
+                    # only want to append neighbour if it hits these conditions
+                    # 1. is the tile within our grid
+                    # 2. is the tile a land tile
+                    # 3. has the tile been visited already  
+
+                    if(
+                        newRow in range(maxRows) and
+                        newCol in range(maxCols) and
+                        (newRow,newCol) not in visited and 
+                        grid[newRow][newCol] == "1"
                     ):
-                        q.append((newRow, newCol))
                         visited.add((newRow, newCol))
+                        q.append((newRow,newCol))
 
-        #bfs on each node that hasn't been visited
-        #if the bfs discovers other '1's in the grid that haven't been visited
-        # it means previous BFS attempts didn't connect to this tile
-        # as such it is a new island
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == "1" and (row,col) not in visited:
-                    bfs(row,col)
+
+        for row in range(maxRows):
+            for col in range(maxCols):
+                if grid[row][col] == '1' and (row, col) not in visited:
                     numIslands += 1
+                    bfs(row, col)
+        
         return numIslands
 
-                    
-                    
+
+
+
+        
 
 
 
