ebc28218624978da3ce437d845b568cf9e173e5b
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-08-21 22:57:21 -0400

    0152-MAXIMUM-PRODUCT-SUBARRAY Stats: Time: 11 ms (48.47%), Space: 18.2 MB (96.36%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -3,14 +3,17 @@ class Solution:
         res = max(nums)
         curMax, curMin = 1,1
 
+        # for every num n compute the max, and minimum
+        # store the minimum in the case of a negative number
         for n in nums:
+            #reading in a 0 is like resetting the subarray
             if(n == 0):
                 curMin, curMax = 1,1
                 continue
 
             temp = curMax * n
             curMax = max(curMax * n, curMin * n, n) # n itself ~ [-1, 8]
-            curMin = min(curMin * n, temp, n)
+            curMin = min(curMin * n, temp, n) #temp (old curMax) in the case [-1, 8], n in the case [-1, -8]
 
             res = max(curMax, res)
         
