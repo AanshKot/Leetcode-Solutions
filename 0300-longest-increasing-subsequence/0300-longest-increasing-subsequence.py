03c0c4af271e5ef779ef4c35cf0e0940594a8a0b
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-08-31 12:50:18 -0400

    0300-LONGEST-INCREASING-SUBSEQUENCE Stats: Time: 1788 ms (33.56%), Space: 18 MB (55%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -5,11 +5,13 @@ class Solution:
 
         for i in range(len(nums) - 1, -1, -1):
             # for every number after the current pointer i, check if its greater than the current num at i
-            # if it is, return the max of  
             for j in range(i + 1, len(nums)):
+                #if nums[j] is greater than nums[i] it can be a part of the LIS from i onwards
                 if nums[j] > nums[i]:
                     LIS[i] = max(LIS[i], 1 + LIS[j])
         
+         # The result is the maximum value in LIS, since the longest increasing subsequence
+        # can start at any index in the array.
         return max(LIS)
         
         
