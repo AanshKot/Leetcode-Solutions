6af0b04aa2985af82ae9d21278b75c6061b38081
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-09-05 14:12:53 -0400

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 17.6 MB (95.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,16 @@
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        one,two = 1,1
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # 0 stairs - 0 methods
+        # 1 stair - 1 method 
+        # 2 stairs - 2 methods
+        # 3 stairs - 3 methods
+        # 4 stairs - 5 methods:  1 *4, 1 + 1 + 2, 1 + 3, 2 + 2, 2 + 1 + 1
+        dp = [1, 1]
 
-        for i in range(n-1):
-            temp = one
-            one = one + two
-            two = temp
-        return one
+        # bottom up tabulation
+        for i in range(2, n + 1):
+            dp.append(dp[i - 1] + dp[i-2])
+
+
+        return dp[n]
+
