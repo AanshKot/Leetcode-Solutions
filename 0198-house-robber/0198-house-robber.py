7c53140c0090776486cb1825252c09312c700681
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-09-10 20:06:11 -0400

    0198-HOUSE-ROBBER Stats: Time: 0 ms (100%), Space: 17.8 MB (54.01%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,25 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        #memoization - store the results of the prev sub problem 
-        memo = {} #stores max profit from index i onwards
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
         
-        def dfs(index):
-            # if reached end of nums
-            if index >= len(nums):
-                return 0
 
-            # if already calculated max profit from robbing from the house further down the street
-            # i.e. already calculated subproblem
-            if index in memo:
-                return memo[index]
-            
-            rob_first = nums[index] + dfs(index + 2)
-            skip_first = dfs(index + 1)
-
-            #set the max profit of the current index to equal to the max profit of either skipping or robbing this first house
-            memo[index] = max(rob_first, skip_first)
-            return memo[index]
-
-        return dfs(0)
             
             
