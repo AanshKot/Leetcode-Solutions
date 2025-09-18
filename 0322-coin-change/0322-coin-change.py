659ae3d6c4ebe7279d800cc08b216487af0bce68
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-09-18 11:07:19 -0400

    0322-COIN-CHANGE Stats: Time: 900 ms (32.35%), Space: 18 MB (87.49%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,20 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        #bottom up approach
-        dp = [float('inf')] * (amount + 1) # going from 0... amount
+        if amount == 0:
+            return 0
+
+        dp = [float('inf')] * (amount + 1)
         dp[0] = 0
 
-        #what is the minimum number of coins for us to sum to someAmount
-        for currAmount in range(1, amount + 1):
-            #go through every coin value 
-            for currCoinVal in coins:
-                #only continue on the search if the currAmount - currCoinValue is greater than 0
-                # i.e. 4 - 5 < 0 therefore cannot add up to amount with this combo of coins
-                if currAmount - currCoinVal >= 0: #continue searching
-                    dp[currAmount] = min(dp[currAmount], 1 + dp[currAmount - currCoinVal])
+        for i in range(amount + 1):
+            for j in range(len(coins)):
+                coinVal = coins[j]
+                if i - coinVal < 0:
+                    continue
 
-        
-        return dp[amount] if dp[amount] != float('inf') else -1
-
-            
+                dp[i] = min(dp[i - coinVal] + 1, dp[i])
+    
+        if dp[amount] == float('inf'):
+            return -1
+        return dp[amount]
         
