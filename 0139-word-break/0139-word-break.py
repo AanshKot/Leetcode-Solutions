223bed4cefa111e5377c05f5c0fa31ef946f79b1
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-09-19 22:12:36 -0400

    0139-WORD-BREAK Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,27 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [False] * (len(s) + 1)
-        
-        #base case: last char of s (the empty char) can always be completed by any kind of wordDict
-        # if we get to the end of the string, len(s) this represents the problem that is always solved
-        # segmented entire string successfully (since its empty string)
+
+        # any dictionary can fill in the empty string
         dp[len(s)] = True
 
-        # iterate backwards through the string
         for i in range(len(s) - 1, -1, -1):
+            if dp[i]:
+                return True
             for word in wordDict:
-                if i + len(word) <= len(s) and s[i : i + len(word)] == word:
-                    # the base case last char of s (empty char) ~ at len(s) can always be broken up 
-                    # lets say word is leetcode
-                    #                      ^ ~ pointer 'i' is at position 4, worddict word ~ 'code' length is 4
-                    # remember base case is dp[8] = true
-                    # dp[4] = dp[4 + 4]
+                if i + len(word) < len(s) and s[i: i + len(word)] == word:
                     dp[i] = dp[i + len(word)]
 
-                if dp[i]:
-                    break
-
-
+        
         return dp[0]
-
         
