b834756647496498ee6b0138bfb32cee7598424b
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-10-05 16:27:21 -0400

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 18.1 MB (10.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,21 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        row = [1] * n # this represents the bottom row
+        memo = {}
+        def dp(row, col):
+            # base cases
+            if row > m - 1 or col > n - 1:
+                return 0
+            
+            if row == m - 1 or col == n - 1:
+                return 1
+            
+            if (row,col) in memo:
+                return memo[(row,col)]
+            
+            #decision (branch off point)
+            memo[(row,col)] = dp(row + 1, col) + dp(row, col + 1)
+            return memo[(row, col)]
 
-        # go through all rows
-        for i in range(m-1):
-            newRow = [1] * n #this new row is above the old row
-
-            # last value in every row is always going to be 1, rightmost value in every row is going to be 1 skip computing it
-            # column index within the current row
-            for j in range(n - 2, -1, -1): #iterate through the current row
-                #newRow[j] = value to the right of the current entry + value below the current entry
-                #val to the right of cur entry
-                right = newRow[j + 1]
-                #old row (row we computed before) val below the current entry
-                below = row[j]
-                newRow[j] = right + below
-            row = newRow # row is going to be set to the row above it
-        
-        return row[0]
-        
+        return dp(0,0)
