c2595e4c3f5f73ce5cfe2b417d4a248858f89614
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2025-10-07 10:02:34 -0400

    0309-BEST-TIME-TO-BUY-AND-SELL-STOCK-WITH-COOLDOWN Stats: Time: 7 ms (41.86%), Space: 19 MB (29.87%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,30 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = {} # key = (i, buying) val = max_profit
+        #teamPark approach
+        memo = {}
 
-        def dfs(i, buying):
-            if i >= len(prices):
+        def dp(i, isBuying):
+            # base cases
+            if i > len(prices) - 1:
                 return 0
-            if (i, buying) in dp:
-                return dp[(i,buying)]
             
-            cooldown = dfs(i + 1, buying) # we can always choose to have a cooldown day, meaning neither buying or selling
-
-            if buying:
-                #if we choose to buy on this day, our profit -= prices[i] where i is the current day we are buying on
-                buy = dfs(i + 1, False) - prices[i]
-                dp[(i, buying)] = max(buy, cooldown)
+            if (i, isBuying) in memo:
+                return memo[(i,isBuying)]
             
-            else:
-                #if we choose to sell on the current day, our profit += prices[i] where i i sthe current day we are selling on
-                # if we sell, we have to take a cooldown day, i.e. i + 2
-                # if we sold that means we made some money hence + prices[i]
-                sell = dfs(i + 2, True) + prices[i]
-                dp[(i, buying)] = max(sell, cooldown)
+            cooldown = dp(i+1, isBuying)
             
-            return dp[(i, buying)]
+            # decision stage
+            if isBuying:
+                buy = dp(i + 1, not isBuying) - prices[i]
+                memo[(i, isBuying)] = max(buy, cooldown)
+            
+            if not isBuying:
+                sell = dp(i + 2, not isBuying) + prices[i]
+                memo[(i,isBuying)] = max(sell, cooldown)
+            
+            return memo[(i, isBuying)]
         
-        return dfs(0, True)
+        return dp(0, True)
 
 
         
