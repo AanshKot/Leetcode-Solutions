8baf70d41060c5c029d0638cb84c9f1cc43061ae
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2026-01-06 14:30:47 -0500

    0424-LONGEST-REPEATING-CHARACTER-REPLACEMENT Stats: Time: 181 ms (24.03%), Space: 19.7 MB (16.47%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,34 +1,38 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        freqMap = {}
         l = 0
         res = 0
-        freqMap = {}
 
-        # len of sliding window: r - l + 1
         for r in range(len(s)):
-            freqMap[s[r]] = 1 + freqMap.get(s[r],0)
+            freqMap[s[r]] = freqMap.get(s[r], 0) + 1
+            
+            #dominant char freq
+            domChar = 0
+            for char in freqMap.keys():
+                domChar = max(domChar, freqMap[char])
+            
 
-            dominantCharFreq = 0
-            for i in freqMap:
-                dominantCharFreq = max(dominantCharFreq, freqMap[i])
-                
-            # windowLength - the dominant character frequency of appearances > k then we want to reduce the length of the window until it reaches valid state
-            while (r - l + 1) - dominantCharFreq  > k:
-                # move our window right
-                # 1. decrement the frequency of the character we are leaving out (s[l])
-                # 2. move the left pointer to the right by 1
+            # shift the window until the length of the substring (window) - the frequency of our most popular character is equal to the number of possible replacements we can make
+            # new window will start when 
+            # theres no new dominant char because the window is getting smaller
+            #We donât recalculate domChar when shrinking because the algorithm only needs a non-decreasing upper bound on the dominant frequency; recomputing would add work but provide no additional correctness.
+            while (r - l + 1) - domChar > k:
+                # while sliding the window need to decrement frequency of readOut char 
+                # we are just moving the left pointer the end point of the window
+
+                # don't need to check for new domChar because we already found the greatest possible length of the window from left to right
+                # case a new net from the old net's end point (r) 
+
+                # left pointer is increasing until (r - l) - domChar 
 
                 freqMap[s[l]] = freqMap[s[l]] - 1
                 l += 1
-
+            
             res = max(res, r - l + 1)
-
         return res
 
+                
 
+                
+        
