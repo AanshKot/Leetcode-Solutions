01f755f92af4218819e43892ecfea36dbd5ff04b
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2026-01-12 09:25:07 -0500

    0019-REMOVE-NTH-NODE-FROM-END-OF-LIST Stats: Time: 0 ms (100%), Space: 19.4 MB (5.56%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,11 +5,15 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # reverse linked list then iterate runner to node before the nth node from the start
         reversedLinkedList = self.reverseLinkedList(head)
         if n == 1:
             reversedLinkedList = reversedLinkedList.next
         else:
+            #prev is already at position 1
             prev = reversedLinkedList
+            #need to stop at node before nth node, position = n - 1
+            # (n - 1) - 1 = n - 2
             for _ in range(n - 2):
                 prev = prev.next
 
