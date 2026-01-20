940e07979bcf412c340af8ec122892c68da37210
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2026-01-20 10:58:08 -0500

    0543-DIAMETER-OF-BINARY-TREE Stats: Time: 2 ms (92.98%), Space: 22.3 MB (9.56%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -7,17 +7,16 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         res = [0]
-
         def dfs(root):
             if not root:
                 return 0
-            
+
             leftHeight = dfs(root.left) #maxDepth of leftSubtree
             rightHeight = dfs(root.right) #maxDepth of rightSubtree
-
-            res[0] = max(res[0], leftHeight + rightHeight) #combined leftHeight + rightHeight gives max diameter
-
-            return 1 + max(leftHeight,rightHeight) #return the max depth of the subtree
+            
+            res[0] = max(res[0], leftHeight + rightHeight) #diameter is sum of max depths of both subtrees
+            return 1 + max(leftHeight, rightHeight) # max depth of subtree is one side of the triangle diameter / \
+        
 
         dfs(root)
         return res[0]
