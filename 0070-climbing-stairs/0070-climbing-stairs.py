7384fb3422798c2ec82c90eb129ca83d5b4af1e8
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2026-06-08 16:59:27 -0400

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 19.1 MB (86.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,22 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # 0 stairs - 0 methods
-        # 1 stair - 1 method 
-        # 2 stairs - 2 methods
-        # 3 stairs - 3 methods
-        # 4 stairs - 5 methods:  1 *4, 1 + 1 + 2, 1 + 3, 2 + 2, 2 + 1 + 1
-        dp = [1, 1]
+        if n == 0:
+            return 0
 
-        # bottom up tabulation
-        for i in range(2, n + 1):
-            dp.append(dp[i - 1] + dp[i-2])
+        mappedClimbWays = {}
 
+        def topDown(n):
+            # base case if num steps equal to 1
+            if n <= 1:
+                return 1
 
-        return dp[n]
+            if n in mappedClimbWays.keys():
+                return mappedClimbWays[n]
+            
+            res = topDown(n-1) + topDown(n-2)
+            mappedClimbWays.update({n:res})
+            return res
+        
+        return topDown(n)
 
+        
