e93981c7cfe669b5a53fb63faee39a80e1020977
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2026-06-11 12:36:01 -0400

    0213-HOUSE-ROBBER-II Stats: Time: 0 ms (100%), Space: 19.5 MB (10.81%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,29 +1,24 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if(len(nums) == 1):
-            return nums[0] # if the length of nums is 1 then we can't do the max compare because length of array is 1, so nums[1:] returns the empty array and same for nums[:len(nums) - 1]
+            return nums[0]
         
-        return max(self.houseRobber1(nums[1:]), self.houseRobber1(nums[:len(nums) - 1]))
+        return max(self.houseRobber1(nums[1:]), self.houseRobber1(nums[:-1]))
 
     def houseRobber1(self,nums):
         memo = {}
 
-        def dfs(index):
-            # max profit for a given index
-            if index in memo:
-                return memo[index]
-            
-            #second base case
-            #0 profit if pass all houses
-            if index >= len(nums):
+        def traverseStreet(i):
+            if i >= len(nums):
                 return 0
-
-            robFirst = nums[index] + dfs(index + 2)
-            skipFirst = dfs(index + 1)
-
-            memo[index] = max(robFirst, skipFirst)
-            return memo[index]
-
-        return dfs(0)
             
+            if i in memo:
+                return memo[i]
+            
+            # max of rob + skip adjacent vs skip current and traverse to next (can choose to rob that house or skip)
+            memo[i] = max(nums[i] + traverseStreet(i+2), traverseStreet(i+1))
+            return memo[i]
+        
+        return traverseStreet(0)
+
 
